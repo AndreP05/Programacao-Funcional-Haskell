31029ff0a6535711c6bb41c42088c4d1b0b4a49a
André Pacheco dos Santos <51138775+AndreP05@users.noreply.github.com>
2022-10-20 18:41:19 -0300

    Add files via upload

--- Trabalho 9.clj
+++ Trabalho 9.clj
@@ -5,14 +5,14 @@
   (nth arg (- (count arg) 1))
 )
 
-(println (ultimo [2 1 6 8 9]))
+(println "Func 1. Entrada: [2 1 6 8 9] 2. Saida: " (ultimo [2 1 6 8 9]))
 
 ; 2. Utilizando a linguagem Clojure, crie uma função chamada penultimo que receba uma lista e  devolva  o  penúltimo  elemento  desta  lista  usar as  funções  já  prontas  e disponíveis para esta mesma finalidade na linguagem Clojure.
 (defn penultimo [arg] 
   (nth arg (- (- (count arg) 1) 1))
 )
 
-(println (penultimo [2 1 6 8 9]))
+(println "Func 2. Entrada: [2 1 6 8 9] 2. Saida: " (penultimo [2 1 6 8 9]))
 
 ; 3. Utilizando a linguagem Clojure, crie uma função chamada elementoN que receba uma lista e um inteiro N e devolva o  elemento que  está na  posição N desta lista usar as funções já prontas e disponíveis para esta mesma finalidade na linguagem Clojure.
 (defn elementoN [lista n]
@@ -24,7 +24,7 @@
   )
 )
 
-(println (elementoN [2 1 6 8 9] 2))
+(println "Func 3. Entrada: [2 1 6 8 9] 2. Saida: " (elementoN [2 1 6 8 9] 2))
 
 ; 4. Utilizando  a  linguagem Clojure,  crie  uma função  chamada  inverso  que  receba uma  lista  e devolva esta lista com as posições dos elementos invertidas. Por exemplo recebe [1,2,3] e devolve [3,2,1]. Sem usar as funções já prontas e disponíveis para esta mesma finalidade na linguagem Clojure.
 (defn inverso [arg]
@@ -35,4 +35,19 @@
   )
 )
 
-(println (inverso [5 4 3 2 1]) )
+(println "Func 4. Entrada: [5 4 3 2 1]. Saida: " (inverso [5 4 3 2 1]) )
+
+; 5. Utilizando a  linguagem Clojure, crie uma função chamada  mdc que receba  dois inteiros e devolve o mínimo divisor comum entre eles.  Sem usar as funções já prontas e disponíveis para esta mesma finalidade na linguagem Clojure.  
+
+(defn mdc [arg1 arg2]
+  (loop [alvo (max arg1 arg2) ]
+    (if (or 
+         (identical? alvo 1) 
+         (and (zero? (mod arg1 alvo)) (zero? (mod arg2 alvo))) 
+        )
+        alvo (recur (dec alvo))
+    )
+  )
+)
+
+(println "Func 5. Entrada: 8 22. Saida: " (mdc 8 22))
